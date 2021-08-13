bc5bee7f3b1a10be58d49a84403400a628943b8b
JohnWest <johnwest@umich.edu>
2021-08-13 03:29:00 -0500

    Added ArrayList of MedallionCombos to handle unknown sizes.

--- src/noHitMedallionBoard/NoHitMedallionBoard.java
+++ src/noHitMedallionBoard/NoHitMedallionBoard.java
@@ -4,10 +4,14 @@ import java.awt.*;
 import java.awt.BorderLayout;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import javax.swing.*;
 import javax.swing.BorderFactory;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout.ParallelGroup;
+import javax.swing.GroupLayout.SequentialGroup;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -15,6 +19,7 @@ public class NoHitMedallionBoard {
 
 	private JFrame frame;
 	private JPanel panel;
+	private ArrayList<MedallionCombo> medallionArrayList;
 	private noHitMedallionBoard.MedallionCombo zootCombo;
 	private noHitMedallionBoard.MedallionCombo mmCombo;
 	private noHitMedallionBoard.MedallionCombo wwCombo;
@@ -28,93 +33,71 @@ public class NoHitMedallionBoard {
 		panel = new JPanel();
 		GroupLayout layout = new GroupLayout(panel);
 		
-		zootCombo = new noHitMedallionBoard.MedallionCombo("The Legend of Zelda: Ocarina of Time",
-				"/Volumes/Seagate 2 TB Storage/Ocarina_of_Time.png");
-		mmCombo = new noHitMedallionBoard.MedallionCombo("The Legend of Zelda: Majora's Mask",
-				"/Volumes/Seagate 2 TB Storage/Majoras_Mask.png");
-		wwCombo = new noHitMedallionBoard.MedallionCombo("The Legend of Zelda: Wind Waker",
-				"/Volumes/Seagate 2 TB Storage/Wind_Waker.png");
-		tpCombo = new noHitMedallionBoard.MedallionCombo("The Legend of Zelda: Twilight Princess",
-				"/Volumes/Seagate 2 TB Storage/Fusedshadow.png");
-		ssCombo = new noHitMedallionBoard.MedallionCombo("The Legend of Zelda: Skyward Sword",
-				"/Volumes/Seagate 2 TB Storage/SS_Zelda.png");
-		botwCombo = new noHitMedallionBoard.MedallionCombo("The Legend of Zelda: Breath of the Wild",
-				"/Volumes/Seagate 2 TB Storage/Hestus_Gift.png");
+		medallionArrayList = new ArrayList<MedallionCombo>(); 
+		
+		medallionArrayList.add(new noHitMedallionBoard.MedallionCombo("The Legend of Zelda: Ocarina of Time",
+								"/Volumes/Seagate 2 TB Storage/Ocarina_of_Time.png"));
+		medallionArrayList.add(new noHitMedallionBoard.MedallionCombo("The Legend of Zelda: Majora's Mask",
+								"/Volumes/Seagate 2 TB Storage/Majoras_Mask.png"));
+		medallionArrayList.add(new noHitMedallionBoard.MedallionCombo("The Legend of Zelda: Wind Waker",
+								"/Volumes/Seagate 2 TB Storage/Wind_Waker.png"));
+		medallionArrayList.add(new noHitMedallionBoard.MedallionCombo("The Legend of Zelda: Twilight Princess",
+								"/Volumes/Seagate 2 TB Storage/Fusedshadow.png"));
+		medallionArrayList.add(new noHitMedallionBoard.MedallionCombo("The Legend of Zelda: Skyward Sword",
+								"/Volumes/Seagate 2 TB Storage/SS_Zelda.png"));
+		medallionArrayList.add(new noHitMedallionBoard.MedallionCombo("The Legend of Zelda: Breath of the Wild",
+								"/Volumes/Seagate 2 TB Storage/Hestus_Gift.png"));
 
 		layout.setAutoCreateGaps(true);
 		layout.setAutoCreateContainerGaps(true);
 		
-		layout.setHorizontalGroup(
-				layout.createSequentialGroup()
-						.addGroup(layout.createParallelGroup()
-								.addGroup(layout.createParallelGroup(Alignment.CENTER)
-										.addComponent(zootCombo.getMedallionTextArea())
-										.addComponent(zootCombo.getMedallionButton())
-								)
-								.addGroup(layout.createParallelGroup(Alignment.CENTER)
-										.addComponent(ssCombo.getMedallionTextArea())
-										.addComponent(ssCombo.getMedallionButton())
-								)
-						)
-						.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED, 20, 40)
-						.addGroup(layout.createParallelGroup()
-								.addGroup(layout.createParallelGroup(Alignment.CENTER)
-										.addComponent(mmCombo.getMedallionTextArea())
-										.addComponent(mmCombo.getMedallionButton())
-								)
-								.addGroup(layout.createParallelGroup(Alignment.CENTER)
-										.addComponent(botwCombo.getMedallionTextArea())
-										.addComponent(botwCombo.getMedallionButton())
-								)
-						)
-						.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED, 20, 40)
-						.addGroup(layout.createParallelGroup()
-								.addGroup(layout.createParallelGroup(Alignment.CENTER)
-										.addComponent(wwCombo.getMedallionTextArea())
-										.addComponent(wwCombo.getMedallionButton())
-								)
-						)
-						.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED, 20, 40)
-						.addGroup(layout.createParallelGroup()
-								.addGroup(layout.createParallelGroup(Alignment.CENTER)
-										.addComponent(tpCombo.getMedallionTextArea())
-										.addComponent(tpCombo.getMedallionButton())
-								)
-						)
-		);
+		SequentialGroup rowSequentialGroup = layout.createSequentialGroup();
 		
-		layout.setVerticalGroup(
-				layout.createSequentialGroup()
-						.addGroup(layout.createParallelGroup()
-								.addGroup(layout.createSequentialGroup()
-										.addComponent(zootCombo.getMedallionTextArea())
-										.addComponent(zootCombo.getMedallionButton())
-								)
-								.addGroup(layout.createSequentialGroup()
-										.addComponent(mmCombo.getMedallionTextArea())
-										.addComponent(mmCombo.getMedallionButton())
-								)
-								.addGroup(layout.createSequentialGroup()
-										.addComponent(wwCombo.getMedallionTextArea())
-										.addComponent(wwCombo.getMedallionButton())
-								)
-								.addGroup(layout.createSequentialGroup()
-										.addComponent(tpCombo.getMedallionTextArea())
-										.addComponent(tpCombo.getMedallionButton())
-								)
-						)
-						.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 60, 60)
-						.addGroup(layout.createParallelGroup()
-								.addGroup(layout.createSequentialGroup()
-										.addComponent(ssCombo.getMedallionTextArea())
-										.addComponent(ssCombo.getMedallionButton())
-								)
-								.addGroup(layout.createSequentialGroup()
-										.addComponent(botwCombo.getMedallionTextArea())
-										.addComponent(botwCombo.getMedallionButton())
-								)
-						)
-		);
+		// Iterate over the ArrayList to create horizontal layout groupings
+		for (int i = 0; (i < medallionArrayList.size()) && (i < 4); i++) {
+			
+			ParallelGroup columnParallelGroup = layout.createParallelGroup(Alignment.CENTER);
+			
+			for (int j = i; j < medallionArrayList.size(); j=j+4) {
+				
+				MedallionCombo currentMedallionCombo = medallionArrayList.get(j);
+				
+				ParallelGroup medallionParallelGroup = layout.createParallelGroup(Alignment.CENTER);
+				medallionParallelGroup.addComponent(currentMedallionCombo.getMedallionTextArea());
+				medallionParallelGroup.addComponent(currentMedallionCombo.getMedallionButton());
+				
+				columnParallelGroup.addGroup(medallionParallelGroup);
+			}
+			
+			rowSequentialGroup.addGroup(columnParallelGroup);
+			rowSequentialGroup.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED, 20, 40);
+		}
+		
+		layout.setHorizontalGroup(rowSequentialGroup);
+		
+		SequentialGroup columnSequentialGroup = layout.createSequentialGroup();
+		
+		// Iterate over the ArrayList to create vertical layout groupings
+		for (int i = 0; i < medallionArrayList.size(); i=i+4) {
+			
+			ParallelGroup rowParallelGroup = layout.createParallelGroup(Alignment.CENTER);
+			
+			for (int j = i; (j < i + 4) && (j < medallionArrayList.size()); j++) {
+				
+				MedallionCombo currentMedallionCombo = medallionArrayList.get(j);
+				
+				SequentialGroup medallionSequentialGroup = layout.createSequentialGroup();
+				medallionSequentialGroup.addComponent(currentMedallionCombo.getMedallionTextArea());
+				medallionSequentialGroup.addComponent(currentMedallionCombo.getMedallionButton());
+				
+				rowParallelGroup.addGroup(medallionSequentialGroup);
+			}
+			
+			columnSequentialGroup.addGroup(rowParallelGroup);
+			columnSequentialGroup.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 60, 60);
+		}
+		
+		layout.setVerticalGroup(columnSequentialGroup);
 		
 		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		panel.setLayout(layout);
@@ -124,8 +107,11 @@ public class NoHitMedallionBoard {
 		frame.addComponentListener(new ComponentAdapter() {
 			public void componentResized(ComponentEvent componentEvent) {
 				
-				zootCombo.frameResizeEvent();
-				//mmPanel.frameResizeEvent();
+				for (Iterator<MedallionCombo> iterator = medallionArrayList.iterator(); iterator.hasNext();) {
+					MedallionCombo medallionCombo = (MedallionCombo) iterator.next();
+					
+					medallionCombo.frameResizeEvent();
+				}
 			}
 		});
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
