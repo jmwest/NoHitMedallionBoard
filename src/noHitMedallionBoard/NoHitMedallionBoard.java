b9b708891f9e3608f326b4ac6ab81945e6c1ec1e
JohnWest <johnwest@umich.edu>
2021-09-18 06:39:43 -0500

    Reworking save/load file so it doesn't conflict.

--- src/noHitMedallionBoard/NoHitMedallionBoard.java
+++ src/noHitMedallionBoard/NoHitMedallionBoard.java
@@ -13,10 +13,13 @@ import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 
@@ -50,7 +53,7 @@ public class NoHitMedallionBoard implements ActionListener {
     private String userLoadFilePathString = System.getProperty("user.dir") + System.getProperty("file.separator")
 											+ "resources" + System.getProperty("file.separator");
     private String userSaveFilePathString = System.getProperty("user.dir") + System.getProperty("file.separator")
-    										+ "resources" + System.getProperty("file.separator") + "usersave.txt";
+    										+ "resources" + System.getProperty("file.separator") + "No_Hit_Medallion_usersave.txt";
     private boolean useZeldaPreset = false;
     
 	private JMenuBar menuBar;
@@ -212,8 +215,8 @@ public class NoHitMedallionBoard implements ActionListener {
 	
 	public static void main(String[] args) {
 		
-		/*
-		File file = new File(System.getProperty("user.dir") + "/log.txt");
+		
+		File file = new File(System.getProperty("user.dir") + "/No_Hit_Medallion_log.txt");
         FileOutputStream fos;
 		try {
 			fos = new FileOutputStream(file);
@@ -227,7 +230,7 @@ public class NoHitMedallionBoard implements ActionListener {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		*/
+		
 		
 		System.out.println("Working dir:  " + System.getProperty("user.dir"));
 		
